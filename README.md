7d6e40ce96079fc9f71b4e88b6dbb6d76bbcb39d
funabashi800 <8118524@ed.tus.ac.jp>
2019-01-04 21:08:44 +0900

    add non liner poisson equation

--- README.md
+++ README.md
@@ -13,5 +13,13 @@
  $ docker exec -it [name] /bin/bash -l
  ```
 
+### Boundary function for Dirichlet condition
+
+![2019-01-04 18 40 29](https://user-images.githubusercontent.com/27273842/50681867-499d8980-1050-11e9-9008-99c54d6142d6.png)
+
+ ```python
+f = Expression(’x[0]>=0 && x[1]>=0 ? pow(x[0], 2) : 2’, degree=2)
+ ```
+
 ![poisson](https://user-images.githubusercontent.com/27273842/50679244-0c33fe80-1046-11e9-9e0e-53160b3e77c4.png)
 
--- labrace.py
+++ labrace.py
@@ -12,25 +12,57 @@ from fenics import *
 import matplotlib.pyplot as plt
 
 # Create mesh and define function space
+# Square get divided into 8 x 8
 mesh = UnitSquareMesh(8, 8)
+
+# Create a finite element function space
+# Second argue 'P' imply the standard Largrange family of element.
 V = FunctionSpace(mesh, 'P', 1)
 
 # Define boundary condition
+
+"""
+u_D = Expression(formula, degree=1)
+
+Note: formula must be written in C++, and then automatically turned into an efficient function
+e.g. a**2 -> pow(a, 2) corresponding to Cmath library!
+About degree(third param): a higher degree must be used for the expression (one or two)
+
+In 3D expression, you can use x[2] as z-axis
+"""
+
 u_D = Expression('1 + x[0]*x[0] + 2*x[1]*x[1]', degree=2)
 
+
+# return a boolean value:True if the given point x lies on the Dirichlet # boundary and False otherwise
 def boundary(x, on_boundary):
     return on_boundary
 
+# Boundary condition
+# third arg shows which points belong to boundary
+# boudary function will be called for every discreate point in the mesh
 bc = DirichletBC(V, u_D, boundary)
+
+"""
+# same as above function
+def boundary(x):
+    return x[0] == 0 or x[1] == 0 x[0] == 1 or x[1] == 1
+"""
 
 # Define variational problem
 u = TrialFunction(V)
 v = TestFunction(V)
+
+"""
+f = Expression('-6', degree=0)
+# same as below object
+"""
 f = Constant(-6.0)
 a = dot(grad(u), grad(v))*dx
 L = f*v*dx
 
-# Compute solution
+# Done with defining all variatiocal problem and boundary condition
+# Compute solution with Fenics
 u = Function(V)
 solve(a == L, u, bc)
 
