9a2ac82c358a178ec708b0bc9b6e0268bf72ba45
funabashi800 <8118524@ed.tus.ac.jp>
2019-01-05 12:43:31 +0900

    add 3d plot with matplotlib

--- poisson.py
+++ poisson.py
@@ -9,6 +9,7 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 
 # Warning: from fenics import * will import both `sym` and
 # `q` from FEniCS. We therefore import FEniCS first and then
@@ -51,17 +52,39 @@ F = q(u)*dot(grad(u), grad(v))*dx - f*v*dx
 # Compute solution
 solve(F == 0, u, bc)
 
-
 arr = np.array(u.vector()[:])
 np.reshape(arr, (9, 9))
 
-plot(u)
-plt.savefig("non-liner-poisson.png")
+# output potential calculated with FEniCS
+"""
+output = open("out.txt", "w+")
+cordinate = mesh.coordinates()
+u_array = u.vector()[:]
+for i in range(mesh.num_vertices()-1):
+    output.write('x:%8g, y:%8g, potential %g\n' % (cordinate[i][0], cordinate[i][1], u_array[i]))
+output.close()
+"""
 
-# Save solution to file in VTK format
-vtkfile = File('poisson/nonliner-poisson.pvd')
-vtkfile << u
-
+# plot 3d figure in x-y electro static potential with matplot lib
+x = np.array([])
+y = np.array([])
+z = np.array([])
+u_array = u.vector()[:]
+grid = mesh.coordinates()
+for i in range(mesh.num_vertices()):
+    x = np.append(x, grid[i][0])
+    y = np.append(y, grid[i][1])
+    z = np.append(z, u_array[i])
+X = np.reshape(x, (9,9)) 
+Y = np.reshape(y, (9,9))
+Z = np.reshape(z, (9,9))
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+surf = ax.plot_surface(X ,Y, Z, cmap='bwr', linewidth=0)
+fig.colorbar(surf)
+ax.set_title("Electro Static Potential by FEniCS(PDEs)")
+fig.show()
+plt.savefig("non-liner-poisson3d.png")
 
 # Compute maximum error at vertices. This computation illustrates
 # an alternative to using compute_vertex_values as in poisson.py.
